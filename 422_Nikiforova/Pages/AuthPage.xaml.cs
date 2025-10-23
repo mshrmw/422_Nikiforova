8ac31115eebcff769037b0cf3ea56f2ffa0b836e
mshrmw <juli12021972@gmail.com>
2025-10-23 12:05:03 +0300

    Разработка капчи на странице авторизации

--- 422_Nikiforova/Pages/AuthPage.xaml.cs
+++ 422_Nikiforova/Pages/AuthPage.xaml.cs
@@ -58,11 +58,11 @@ namespace _422_Nikiforova.Pages
                     failedAttempts++;
                     if (failedAttempts >= 3)
                     {
-                        //if (captcha.Visibility != Visibility.Visible)
-                        //{
-                        //    CaptchaSwitch();
-                        //}
-                        //CaptchaChange();
+                        if (captcha.Visibility != Visibility.Visible)
+                        {
+                            CaptchaSwitch();
+                        }
+                        CaptchaChange();
                     }
                     return;
                 }
@@ -73,14 +73,93 @@ namespace _422_Nikiforova.Pages
                     switch (user.Role)
                     {
                         case "User":
-                            //  
+                            NavigationService?.Navigate(new Pages.UserPage());
                             break;
                         case "Admin":
-                            //NavigationService?.Navigate(new Pages.AdminPage());
+                            NavigationService?.Navigate(new Pages.AdminPage());
                             break;
 
                     }
                 }
+            }
+        }
+        public void CaptchaSwitch()
+        {
+            switch (captcha.Visibility)
+            {
+                case Visibility.Visible:
+                    TextBoxLogin.Clear();
+                    PasswordBox.Clear();
+
+                    captcha.Visibility = Visibility.Hidden;
+                    captchaInput.Visibility = Visibility.Hidden;
+                    labelCaptcha.Visibility = Visibility.Hidden;
+                    submitCaptcha.Visibility = Visibility.Hidden;
+
+                    labelLogin.Visibility = Visibility.Visible;
+                    labelPass.Visibility = Visibility.Visible;
+                    TextBoxLogin.Visibility = Visibility.Visible;
+                    PasswordBox.Visibility = Visibility.Visible;
+
+                    ButtonChangePassword.Visibility = Visibility.Visible;
+                    ButtonEnter.Visibility = Visibility.Visible;
+                    ButtonReg.Visibility = Visibility.Visible;
+                    return;
+                case Visibility.Hidden:
+                    captcha.Visibility = Visibility.Visible;
+                    captchaInput.Visibility = Visibility.Visible;
+                    labelCaptcha.Visibility = Visibility.Visible;
+                    submitCaptcha.Visibility = Visibility.Visible;
+
+                    labelLogin.Visibility = Visibility.Hidden;
+                    labelPass.Visibility = Visibility.Hidden;
+                    TextBoxLogin.Visibility = Visibility.Hidden;
+                    PasswordBox.Visibility = Visibility.Hidden;
+
+                    ButtonChangePassword.Visibility = Visibility.Hidden;
+                    ButtonEnter.Visibility = Visibility.Hidden;
+                    ButtonReg.Visibility = Visibility.Hidden;
+                    return;
+            }
+        }
+        public void CaptchaChange()
+        {
+            String allowchar = " ";
+            allowchar = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+            allowchar += "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z";
+            allowchar += "1,2,3,4,5,6,7,8,9,0";
+            char[] a = { ',' };
+            String[] ar = allowchar.Split(a);
+            String pwd = "";
+            string temp = "";
+            Random r = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                temp = ar[(r.Next(0, ar.Length))];
+                pwd += temp;
+            }
+            captcha.Text = pwd;
+        }
+        private void submitCaptcha_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (captchaInput.Text != captcha.Text)
+            {
+                MessageBox.Show("Неверно введена капча", "Ошибка");
+                CaptchaChange();
+            }
+            else
+            {
+                MessageBox.Show("Капча введена успешно, можете продолжить авторизацию", "Успех");
+                CaptchaSwitch();
+                failedAttempts = 0;
+            }
+        }
+        private void textBox_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Copy || e.Command == ApplicationCommands.Cut || e.Command == ApplicationCommands.Paste)
+            {
+                e.Handled = true;
             }
         }
         private void ButtonReg_Click(object sender, RoutedEventArgs e)
